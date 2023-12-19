9052551d5abfbacc4ede4ff883e8de54e345092f
patrickngangyou <130426575+patrickngangyou@users.noreply.github.com>
2023-12-18 21:56:17 -0500

    Remote Update 2 Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 class Hell{
 Public static void main(String args[])
 {
-System.out.println(“updated hello from remote”);
+System.out.println(“updated hello from remote 2nd changes”);
 }
 }
 
