bb5cde7497a426a2419c3c3166bac49d0deea790
patrick <patrickngangyou@gmail.com>
2023-12-20 04:06:56 +0000

    Hello.java from parent

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello
 class Hell{
 Public static void main(String args[])
 {
-System.out.println(“updated hello from child branch”);
+System.out.println(“updated hello from parent branch”);
 }a
 
