559577dfa4c8400f5ee641ea3f21952c9653699f
patrickngangyou <130426575+patrickngangyou@users.noreply.github.com>
2023-12-20 21:19:41 -0500

    remote Update Hello.java from newphew

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 class Hell{
 Public static void main(String args[])
 {
-System.out.println(“updated hello from remote cousin branch”);
+System.out.println(“updated hello from remote newphew branch”);
 }
 }
 
