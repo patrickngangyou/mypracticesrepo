bac9974eb35e1371cea28ca0e86b24e65325245f
patrick <patrickngangyou@gmail.com>
2024-01-05 02:32:16 +0000

    Hello.java merge to master

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello
 class Hell{
 Public static void main(String args[])
 {
-System.out.println(“updated hello from parent branch”);
+System.out.println(“updated hello from parent to merge to parent  branch”);
 }a
 
